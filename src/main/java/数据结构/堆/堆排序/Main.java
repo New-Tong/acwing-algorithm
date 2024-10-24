07d403b4c3b66da78d3c285203344955290a6c78
NewTong <90118167+New-Tong@users.noreply.github.com>
2024-10-24 22:55:36 +0800

    acwing: 2024/10/18 - dorm

---
+++ src/main/java/数据结构/堆/堆排序/Main.java
@@ -0,0 +1,27 @@
+package 数据结构.堆.堆排序;
+
+import java.io.*;
+
+class Main {
+
+    static int N = 100010;
+    static int[] heap = new int[N];
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter printWriter = new PrintWriter(new OutputStreamWriter(System.out));
+
+
+        printWriter.flush();
+        bufferedReader.close();
+        printWriter.close();
+    }
+
+    static void down(int x) {
+        
+    }
+
+    static void up(int x) {
+
+    }
+}
---
+++ src/main/java/数据结构/并查集/合并集合/Main.java
@@ -0,0 +1,42 @@
+package 数据结构.并查集.合并集合;
+
+import java.io.*;
+import java.util.Arrays;
+
+class Main {
+
+    static int N = 100010;
+    static int[] p = new int[N];
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter printWriter = new PrintWriter(new OutputStreamWriter(System.out));
+        int[] array = Arrays.stream(bufferedReader.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        int n = array[0];
+        for (int i = 0; i < n; i++) {
+            p[i] = i;
+        }
+        int m = array[1];
+        while (m -- > 0) {
+            String[] s = bufferedReader.readLine().split(" ");
+            int a = Integer.parseInt(s[1]);
+            int b = Integer.parseInt(s[2]);
+            if (s[0].equals("M")) {
+                p[find(a)] = find(b);
+            } else if (s[0].equals("Q")) {
+                printWriter.println(find(a) == find(b) ? "Yes":"No");
+            }
+        }
+        printWriter.flush();
+        bufferedReader.close();
+        printWriter.close();
+    }
+
+    static int find(int a) {
+        if (a != p[a]) {
+            p[a] = find(p[a]);
+        }
+        return p[a];
+    }
+
+}
