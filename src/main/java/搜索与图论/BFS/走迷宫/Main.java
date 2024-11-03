e60d9c5c093c7a2b4d74fad614544449f050b6fe
NewTong <90118167+New-Tong@users.noreply.github.com>
2024-11-03 21:41:15 +0800

    acwing: 2024/11/3 - dorm

---
+++ src/main/java/搜索与图论/BFS/走迷宫/Main.java
@@ -0,0 +1,15 @@
+package 搜索与图论.BFS.走迷宫;
+
+import java.io.*;
+
+class Main {
+    static BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+    static PrintWriter printWriter = new PrintWriter(new OutputStreamWriter(System.out));
+
+    public static void main(String[] args) throws IOException {
+
+        printWriter.flush();
+        bufferedReader.close();
+        printWriter.close();
+    }
+}
