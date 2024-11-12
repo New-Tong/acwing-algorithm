778692049cebcbcc82dcdf11589f081e661ad0f7
NewTong <90118167+New-Tong@users.noreply.github.com>
2024-11-12 21:28:52 +0800

    2024/11/12-dorm

---
+++ src/main/java/搜索与图论/树与图的深度优先遍历/树的重心/Main.java
@@ -0,0 +1,15 @@
+package 搜索与图论.树与图的深度优先遍历.树的重心;
+
+import java.io.*;
+
+class Main {
+    static BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+    static PrintWriter printWriter = new PrintWriter(new OutputStreamWriter(System.out));
+
+    public static void main(String[] args) throws IOException {
+
+        printWriter.flush();
+        bufferedReader.close();
+        printWriter.close();
+    }
+}
