5330181fb0751af25f7c3865ca1b02d16a227215
New-Tong <1248449622@qq.com>
2025-03-09 18:25:41 +0800

    LeetCode: 2024/3/8

---
+++ src/main/java/牛客/Hot100/找到字符串中所有字母异位词/Main.java
@@ -0,0 +1,50 @@
+package 牛客.Hot100.找到字符串中所有字母异位词;
+
+import java.io.*;
+import java.util.*;
+
+/**
+ @Author NewTong
+ @Date 2025/3/8 -16:08
+ @Description
+ */
+public class Main {
+    private static BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+    private static PrintWriter printWriter = new PrintWriter(new OutputStreamWriter(System.out));
+
+    public static void main(String[] args) throws IOException {
+        String s = bufferedReader.readLine();
+        String p = bufferedReader.readLine();
+        List<Integer> res = findAnagrams(s, p);
+        printWriter.println(res);
+        printWriter.flush();
+        bufferedReader.close();
+        printWriter.close();
+    }
+
+    private static List<Integer> findAnagrams(String s, String p) {
+        List<Integer> res = new ArrayList<>();
+        if (s.length() < p.length()) {
+            return res;
+        }
+        int[] cntP = new int[26];
+        int[] cntS = new int[26];
+        for (char c : p.toCharArray()) {
+            cntP[c - 'a']++;
+        }
+        for (int right = 0; right < s.length(); right++) {
+            cntS[s.charAt(right) - 'a'] ++;
+            int left = right - p.length() + 1;
+            if (left < 0) {
+                continue;
+            }
+            if (Arrays.equals(cntP, cntS)) {
+                res.add(left);
+            }
+            cntS[s.charAt(left) - 'a'] --;
+        }
+        return res;
+    }
+
+
+}
---
+++ src/main/java/牛客/Hot100/无重复字符的最大子串/Main.java
@@ -0,0 +1,44 @@
+package 牛客.Hot100.无重复字符的最大子串;
+
+import java.io.*;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ @Author NewTong
+ @Date 2025/3/8 -15:33
+ @Description
+ */
+public class Main {
+    private static BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+    private static PrintWriter printWriter = new PrintWriter(new OutputStreamWriter(System.out));
+
+    public static void main(String[] args) throws IOException {
+        String str = bufferedReader.readLine();
+        printWriter.println(lengthOfLongestSubstring(str));
+        printWriter.flush();
+        bufferedReader.close();
+        printWriter.close();
+    }
+
+    public static int lengthOfLongestSubstring(String s) {
+        int res = 0, left = 0, right = 0;
+        Set<Character> set = new HashSet<>();
+        while (right < s.length()) {
+            Character c = s.charAt(right);
+            if (set.contains(c)) {
+                // 左边边界往右移动，直到遇到重复的字符
+                Character leftChar = s.charAt(left);
+                set.remove(leftChar);
+                left++;
+            } else {
+                res = Math.max(res, right - left + 1);
+                set.add(c);
+                right++;
+            }
+        }
+        return res;
+    }
+}
