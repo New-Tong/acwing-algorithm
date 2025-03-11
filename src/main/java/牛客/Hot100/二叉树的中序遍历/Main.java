792e37edac6afe6b8fb4f337ff1d6e0597c9036e
New-Tong <1248449622@qq.com>
2025-03-11 17:10:58 +0800

    LeetCode: 2024/3/11

---
+++ src/main/java/牛客/Hot100/二叉树的中序遍历/Main.java
@@ -0,0 +1,80 @@
+package 牛客.Hot100.二叉树的中序遍历;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+/**
+ @Author NewTong
+ @Date 2025/3/11 -16:54
+ @Description
+ */
+
+public class Main {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    private static BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+    private static PrintWriter printWriter = new PrintWriter(new OutputStreamWriter(System.out));
+
+    public static void main(String[] args) throws Exception {
+
+    }
+
+    // 递归
+    private static List<Integer> inorderTraversal(TreeNode root) {
+        if (root == null) {
+            return new ArrayList<>();
+        }
+        List<Integer> list = new ArrayList<>();
+        if (root.left != null) {
+            list.addAll(inorderTraversal(root.left));
+        }
+        int val = root.val;
+        list.add(val);
+        if (root.right != null) {
+            list.addAll(inorderTraversal(root.right));
+        }
+        return list;
+    }
+
+    // 迭代
+    private static List<Integer> inorderTraversal2(TreeNode root) {
+        if (root == null) {
+            return new ArrayList<>();
+        }
+        List<Integer> list = new ArrayList<>();
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode cur = root;
+        while (cur != null || !stack.isEmpty()) {
+            while (cur != null) {
+                stack.push(cur);
+                cur = cur.left;
+            }
+            cur = stack.pop();
+            list.add(cur.val);
+            cur = cur.right;
+        }
+        return list;
+    }
+}
---
+++ src/main/java/牛客/Hot100/合并K个升序链表/Main.java
@@ -0,0 +1,76 @@
+package 牛客.Hot100.合并K个升序链表;
+
+import java.io.*;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+/**
+ @Author NewTong
+ @Date 2025/3/11 -9:17
+ @Description
+ */
+public class Main {
+    private static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+    private static BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+    private static PrintWriter printWriter = new PrintWriter(new OutputStreamWriter(System.out));
+    public static void main(String[] args) throws IOException {
+//        int n = Integer.parseInt(bufferedReader.readLine());
+        ListNode[] lists = new ListNode[2];
+//        for (int i = 0; i < n; i++) {
+//            int[] array = Arrays.stream(bufferedReader.readLine().split(",")).mapToInt(Integer::parseInt).toArray();
+//            ListNode head = new ListNode();
+//            ListNode cur = head;
+//            for (int j = 0; j < array.length; j++) {
+//                cur.next = new ListNode(array[j]);
+//                cur = cur.next;
+//            }
+//            lists[i] = head.next;
+//        }
+        lists[0] = new ListNode(-2, new ListNode(-1, new ListNode(-1, new ListNode(-1))));
+        lists[1] = null;
+        ListNode res = mergeKLists(lists);
+        while (res != null) {
+            printWriter.print(res.val + ",");
+            res = res.next;
+        }
+        printWriter.flush();
+        printWriter.close();
+        bufferedReader.close();
+    }
+
+    public static ListNode mergeKLists(ListNode[] lists) {
+        Queue<ListNode> queue = new PriorityQueue<>(Comparator.comparingInt(a -> a.val));
+        for (ListNode list : lists) {
+            while (list != null) {
+                queue.offer(list);
+                list = list.next;
+            }
+        }
+        ListNode dummy = new ListNode(-1);
+        ListNode cur = dummy;
+        while (!queue.isEmpty()) {
+            cur.next = queue.poll();
+            cur = cur.next;
+        }
+        cur.next = null;
+        return dummy.next;
+    }
+
+}
